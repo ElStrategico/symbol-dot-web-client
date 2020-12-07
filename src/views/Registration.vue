7b073ff5f894f41dea80088fc26ceae1e7c640d0
El Strategico <strategico.dev@gmail.com>
2020-12-07 19:57:53 +0800

    Added Registration view

---
+++ src/views/Registration.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <RegistrationForm />
+    </div>
+</template>
+
+<script>
+import RegistrationForm from "@/components/RegistrationForm";
+
+export default {
+    name: 'Registration',
+    components: {
+        RegistrationForm
+    }
+}
+</script>
