99ef5da0dac1a2f9c21822b0aeca152fde434449
El Strategico <strategico.dev@gmail.com>
2020-12-09 15:24:37 +0800

    Import creatableUser

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,21 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Axios from 'axios'
 import auth from '@/store/modules/auth.js'
+import creatableUser from '@/store/modules/creatable-user.js'
 
 Vue.use(Vuex)
 
+export const HTTP = Axios.create({
+    baseURL: 'http://symbol-dot.local/api/v1/',
+    headers: {
+        Authorization: 'Foo'
+    }
+});
+
 export default new Vuex.Store({
     modules: {
-        auth
+        auth,
+        creatableUser
     }
 })
