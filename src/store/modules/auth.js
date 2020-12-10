a882c94a2dd1e463bbd4aa3f1eb1dfb630fdb2ef
El Strategico <strategico.dev@gmail.com>
2020-12-10 16:47:02 +0800

    Implements

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,18 +1,36 @@
-import Axios from "axios";
+import { HTTP } from '@/http-common';
 
 export default {
     state: {
-        auth: false
+        token: localStorage.getItem('token'),
+        expired: +localStorage.getItem('expired')
     },
     getters: {
         isAuth(state) {
-            return state.auth;
+            return state.token != undefined;
+        },
+        tokenExpired(state) {
+            let currentTime = Math.floor(Date.now() / 1000);
+            return currentTime > state.expired;
+        }
+    },
+    mutations: {
+        setToken(state, token) {
+            localStorage.setItem('token', token);
+            state.token = token;
+        },
+        setExpired(state, expired) {
+            localStorage.setItem('expired', expired);
+            state.expired = expired;
         }
     },
     actions: {
-        async login({ context }) {
-            let response = await Axios.post('login', data);
-            console.log(response);
+        async login({ commit }) {
+            try {
+                let response = await HTTP.post('login', {});
+            } catch (error) {
+                console.log(error.response.data.errors);
+            }
         }
     }
 }
