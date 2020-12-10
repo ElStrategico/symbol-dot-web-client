1f18ef0061435313b9b2310a34e0e19e44782bd4
El Strategico <strategico.dev@gmail.com>
2020-12-10 16:46:44 +0800

    Refactoring

--- src/store/index.js
+++ src/store/index.js
@@ -6,13 +6,6 @@ import creatableUser from '@/store/modules/creatable-user.js'
 
 Vue.use(Vuex)
 
-export const HTTP = Axios.create({
-    baseURL: 'http://symbol-dot.local/api/v1/',
-    headers: {
-        Authorization: 'Foo'
-    }
-});
-
 export default new Vuex.Store({
     modules: {
         auth,
