d89d7918b60df74dfd22bfe457b7f22654d3e79d
El Strategico <strategico.dev@gmail.com>
2020-12-10 16:46:53 +0800

    Refactoring

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
 import Login from '../views/Login.vue';
 import Registration from '../views/Registration.vue';
 
