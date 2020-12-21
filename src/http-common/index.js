ea94e5e08177c7062cd1bcdf225b236516584f23
El Strategico <strategico.dev@gmail.com>
2020-12-21 16:41:50 +0800

    Added logic for work with auth token

--- src/http-common/index.js
+++ src/http-common/index.js
@@ -3,6 +3,6 @@ import Axios from "axios";
 export const HTTP = Axios.create({
     baseURL: 'http://symbol-dot.local/api/v1/',
     headers: {
-        Authorization: 'Foo'
+        Authorization: `bearer ${localStorage.getItem('token')}`
     }
 });
