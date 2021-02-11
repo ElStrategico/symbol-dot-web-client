64249181f0b5e8a97732c145e518da7e32a7ad40
El Strategico <strategico.dev@gmail.com>
2021-02-12 05:04:51 +0800

    Added ErrorAlert

---
+++ src/components/ErrorAlert.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="alert alert-danger" role="alert">
+      {{message}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ErrorAlert",
+  props: {
+    message: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
