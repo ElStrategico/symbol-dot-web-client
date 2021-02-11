dfab587eb682057baab1252463be2875d0d7d14f
El Strategico <strategico.dev@gmail.com>
2021-02-12 05:05:03 +0800

    Added Loader

---
+++ src/components/Loader.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="position-relative form-group">
+    <div class="lds-dual-ring"></div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Loader"
+}
+</script>
