41c9bc355e24c80ff942128e55315c6b3bd9a769
El Strategico <strategico.dev@gmail.com>
2021-02-13 04:59:56 +0800

    Added Settings view

---
+++ src/views/Settings.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="container">
+
+    </div>
+  </div>
+</template>
+
+<script>
+import Title from "@/components/Title";
+
+export default {
+  name: "Settings",
+  components: {Title}
+}
+</script>
+
+<style scoped>
+
+</style>
