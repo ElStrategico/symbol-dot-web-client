764985f6cd90435aaa42cafb43fc118a8c50f9d7
El Strategico <strategico.dev@gmail.com>
2021-02-13 05:00:06 +0800

    Added Contact view

---
+++ src/views/Contact.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container" style="margin-top: 30px;">
+    <div class="row">
+      <div class="col-lg-12">
+        <ContactItem
+            v-for="contact in contacts" :key="contact.id"
+            :contact="contact"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ContactItem from "@/components/ContactItem";
+import {mapGetters} from "vuex";
+
+export default {
+  name: "Contact",
+  components: {ContactItem},
+  computed: mapGetters(['contacts']),
+  mounted() {
+    this.$store.dispatch('fetchContacts');
+  },
+  destroyed() {
+    this.$store.dispatch('clearContacts');
+  }
+}
+</script>
