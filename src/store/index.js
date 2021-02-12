895e2cc67911cfe12e63a1c436e5c542d4ba4f5c
El Strategico <strategico.dev@gmail.com>
2021-02-12 17:28:08 +0800

    Registry security module

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import Axios from 'axios'
-import auth from '@/store/modules/auth.js'
-import creatableUser from '@/store/modules/creatable-user.js'
+import auth from '@/store/modules/auth'
+import security from '@/store/modules/security'
+import creatableUser from '@/store/modules/creatable-user'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
     modules: {
         auth,
-        creatableUser
+        creatableUser,
+        security
     }
 })
