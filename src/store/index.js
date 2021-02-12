0e1d5b8305e745624f20a5c221803db175f4f27f
El Strategico <strategico.dev@gmail.com>
2021-02-13 05:01:08 +0800

    Registry modules

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import Axios from 'axios'
 import auth from '@/store/modules/auth'
+import contact from '@/store/modules/contact'
 import security from '@/store/modules/security'
+import userAvatar from '@/store/modules/user-avatar'
 import creatableUser from '@/store/modules/creatable-user'
 
 Vue.use(Vuex)
@@ -10,7 +12,9 @@ Vue.use(Vuex)
 export default new Vuex.Store({
     modules: {
         auth,
+        contact,
+        security,
+        userAvatar,
         creatableUser,
-        security
     }
 })
