587fa15f3f77cf184b55368d63c3ea8702ce2489
El Strategico <strategico.dev@gmail.com>
2021-02-16 04:49:42 +0800

    Added contactSearchQuery

--- src/store/modules/contact.js
+++ src/store/modules/contact.js
@@ -2,25 +2,47 @@ import {HTTP} from "@/http-common";
 
 export default {
     state: {
-        contacts: []
+        contacts: [],
+        fetchUrl: 'api/v1/contacts',
+        contactSearchQuery: ''
     },
     getters: {
         contacts(state) {
             return state.contacts;
+        },
+        fetchUrl(state) {
+            return state.fetchUrl;
+        },
+        contactSearchQuery(state) {
+            return state.contactSearchQuery;
         }
     },
     mutations: {
+        setContacts(state, contacts) {
+            state.contacts = contacts;
+        },
         addContacts(state, contacts) {
             state.contacts = state.contacts.concat(contacts);
         },
         clearContacts(state) {
             state.contacts = [];
+        },
+        setContactSearchQuery(state, query) {
+            state.contactSearchQuery = query;
         }
     },
     actions: {
-        async fetchContacts({commit}) {
-            let response = await HTTP.get('api/v1/contacts');
+        async fetchContacts({commit, getters}) {
+            let response = await HTTP.get(getters.fetchUrl);
             commit('addContacts', response.data.data);
+        },
+        async searchContacts({commit, getters}) {
+            let response = await HTTP.get('api/v1/searcher/contacts', {
+                params: {
+                    query: getters.contactSearchQuery
+                }
+            });
+            commit('setContacts', response.data);
         },
         clearContacts({commit}) {
             commit('clearContacts');
