13c849da5f8a133d6144a352d9c3153d3283ab74
El Strategico <strategico.dev@gmail.com>
2021-02-16 04:47:11 +0800

    Added CreateContact view

---
+++ src/views/CreateContact.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container" style="margin-top: 30px;">
+    <div style="text-align: left; margin-bottom: 10px;">
+      <router-link class="btn btn-primary" to="/contact">Назад</router-link>
+    </div>
+    <div class="row">
+      <div class="col-lg-12">
+        <ContactForm />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ContactForm from "@/components/ContactForm";
+
+export default {
+  name: "CreateContact",
+  components: {ContactForm}
+}
+</script>
+
+<style scoped>
+
+</style>
