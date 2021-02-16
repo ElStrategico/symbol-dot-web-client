d098a1d59eeca656958a94384fce57a316806f49
El Strategico <strategico.dev@gmail.com>
2021-02-17 05:03:01 +0800

    Added ContactTag component

---
+++ src/components/ContactTag.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="tag">
+    <button class="button mb-2 mr-2 btn" :style="{color: color, background: backgroundColor }">
+      {{name}}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ContactTag",
+  props: {
+    name: String,
+    color: String,
+    backgroundColor: String
+  }
+}
+</script>
+
+<style scoped>
+.tag {
+  display: inline;
+  width: 10px;
+}
+.button {
+  font-size: 10px;
+}
+</style>
